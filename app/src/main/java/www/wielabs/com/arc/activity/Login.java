c7d2dc10dee34e542ebfe23fc90660b9a70df491
sugooi <shaikadil86@gmail.com>
2017-12-08 22:10:05 +0530

    Hey

---
+++ app/src/main/java/www/wielabs/com/arc/activity/Login.java
@@ -0,0 +1,21 @@
+package www.wielabs.com.arc.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import www.wielabs.com.arc.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+
+    }
+
+
+
+}
